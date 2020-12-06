74926f687aaf7742007708d854ba53db2016f3d7
dfc643 <dfc643@126.com>
2020-12-06 16:11:19 +0800

    AppImage Support

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 cp pulseeffectstray /usr/local/bin
 chmod a+x /usr/local/bin/pulseeffectstray
 cp pulseeffectstray.svg /usr/share/pixmaps
-cp pulseeffectstray.desktop /usr/share/applications
+cp pulseeffectstray._desktop /usr/share/applications/pulseeffectstray.desktop
 
 echo "安装完成"
